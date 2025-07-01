8d6bc5fc8814fa6ff497b8da1b1e7772c06e2fd5
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-07-01 12:44:07 +0530

    Added the getRelated product logic in the service

--- backend/src/services/products/productService.js
+++ backend/src/services/products/productService.js
@@ -1,4 +1,4 @@
-const Product = require('../../models/Products');
+const Product = require('../../models/Product');
 
 const searchProducts = async (searchCriteria, req, res) => {
     const products = await Product.find(searchCriteria)
@@ -11,6 +11,45 @@ const searchProducts = async (searchCriteria, req, res) => {
 };
 
 
+const getProductsByCategory = async (baseSku , limit= 10, page = 1) => {
+    const skip = (page-1) * limit;
+    
+    if(!baseSku) {
+        return {
+            error: true,
+          message: 'Base SKU is required'  
+        };
+    }
+    
+    const baseProduct = await Product.findOne({sku: baseSku, isActive: true});
+    if(!baseProduct) {
+        return {
+          error: true, 
+          message: 'Base Product not found or it is not active '  
+        };
+    }
+
+
+    // Fetch related products based on the base product's category
+    // and excluding the base product itself
+
+    const filter = {
+        categoryId: baseProduct.categoryId,
+        isActive: true,
+        sku: { $ne: baseSku }
+    }
+
+    const relatedproducts = await Product.find(filter)
+        .sort({createdAt: -1})
+        .skip(skip)
+        .limit(limit)
+
+    const total = await Product.countDocuments(filter);
+    return { products: relatedproducts, total };
+}
+
+
 module.exports = {
-    searchProducts
+    searchProducts,
+    getProductsByCategory
 }
